26091b6dfb245116c1329c1b6f0afb7abce25360
set <dm.daviles@gmail.com>
2023-11-29 01:20:37 +0100

    Fix detect quotes on token name. Works with mixed quotes

--- src/tokens_init.c
+++ src/tokens_init.c
@@ -34,9 +34,9 @@ int	get_token_type(char *value)
 
 int	ft_isoperator(char	c, int *quotes)
 {
-	if (c == '"')
+	if (c == '"' && !quotes[1])
 		quotes[0] = !quotes[0];
-	else if (c == '\'')
+	else if (c == '\'' && !quotes[0])
 		quotes[1] = !quotes[1];
 	if (quotes[0] || quotes[1])
 		return (0);
@@ -58,14 +58,17 @@ void	*get_next_token(char *line, int *i)
 	if (ft_isoperator(line[*i], quotes))
 		while (ft_isoperator(line[*i], quotes) && line[*i])
 		{
-			 if (line[*i] == '<' || line[*i] == '>' )
+			 if (line[*i] == '<' || line[*i] == '>')
 			 	save_redir_filename(line, i);
 			else
 				*i = *i + 1;
 		}
 	else
+	{
+		*i = *i + 1;
 		while (!ft_isoperator(line[*i], quotes) && line[*i])
 			*i = *i + 1;
+	}
 	aux = ft_substr(line, start, (size_t)(*i - start));
 	if (aux && !check_emptyorspace(aux))
 	{
