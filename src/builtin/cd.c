5b354dac1679667daa7ffb2c36a9bc8b74f334b3
celiamateos <celiamateos36@gmail.com>
2023-12-13 10:45:24 +0100

    updated cd

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -11,14 +11,6 @@
 /* ************************************************************************** */
 #include "../../include/minishell.h"
 
-void    clean_cd(t_shell_sack *sack)
-{
-    if (sack->env->pwd)
-        free(sack->env->pwd);
-    if (sack->env->oldpwd)
-        free(sack->env->oldpwd);
-}
-
 void    free_cd(char *s1, char*s2, char *s3)
 {
     if (s1)
@@ -31,7 +23,7 @@ void    free_cd(char *s1, char*s2, char *s3)
 
 int cd_root(t_shell_sack *sack, char *pwd, char *old_pwd)
 {
-    char *temp;
+    char    *temp;
 
     temp = get_varcontent(pwd);
     if (!temp)
@@ -48,7 +40,7 @@ int cd_root(t_shell_sack *sack, char *pwd, char *old_pwd)
 
 int cd_path(t_shell_sack *sack,  char *pathname, char *pwd, char *old_pwd)
 {
-    char *temp;
+    char    *temp;
 
     temp = get_varcontent(pwd);
     if (!temp)
@@ -71,36 +63,70 @@ int cd_path(t_shell_sack *sack,  char *pathname, char *pwd, char *old_pwd)
     return (0);
 }
 
+int cd_back(t_shell_sack *sack, char *pwd, char *old_pwd)
+{
+    char    *temp;
+    int     pos;
+    int     i;
+
+    temp = get_varcontent(pwd);
+    if (!temp)
+        return (free_cd(pwd, old_pwd, temp), 1);
+    sack->env->oldpwd = ft_strjoin("OLDPWD=", temp);
+    if (!sack->env->oldpwd)
+        return (free_cd(pwd, old_pwd, temp), 1);
+    free (temp);
+
+    pos = ft_strlen(pwd);
+    while (pwd[pos] != '/' && pwd[pos]);
+        pos--;
+    pos--;
+    temp = ft_substr(pwd, 0, pos);
+    if (!temp) 
+        return(1); //SALIDA DE ERRORES-.-
+    pwd = temp;
+    return (0);
+}
+
+/*@brief ALOCA MEMORIA*/
+char *remove_slash(char *path)
+{
+    char    *temp;
+    char    *pathname;
+
+    if (path[ft_strlen(path) - 1] != '/')
+        return (0);
+    pathname = ft_strtrim(path, "/");
+    if (!pathname)
+        return (NULL); //SALIDA DE ERRORES? 
+    return (pathname);
+}
+
 //@brief Intenta acceder a la ruta pasada como parametro, en caso de error muestra un mensaje.
 // SOLO FUNCIONA AL LLAMAR A CD LA PRIMERA VEZ DESPUES DE EJECUTAR MINISHELL, NO SE SI ESPROBLEMA DE ESTO O DEL EXECUTE
-int    cd(t_shell_sack *sack, char *pathname)
+int    cd(t_shell_sack *sack, char *path)
 {
-    int ret;
-    // char *temp;
-    // char *line;
-    // int     len_pwd = ;
-    // int     len_oldpwd;
+    int     ret;
+    char    *pwd = ft_strdup(sack->env->pwd);
+    char    *old_pwd = ft_strdup(sack->env->oldpwd);
+    char    *pathname;
 
-    char *pwd = ft_strdup(sack->env->pwd);
-    char *old_pwd = ft_strdup(sack->env->oldpwd);
-    clean_cd(sack);
-    if (!pathname)
+    if (chdir(path) == -1)
+        return (ft_putstr_fd("cd: No such file or directory\n", 2), 1);
+    if (!path)
         cd_root(sack, pwd, old_pwd);
+    else if (!ft_strncmp(path, "..", 1))
+        cd_back(sack, pwd, old_pwd);
     else
     {   
-        ret = chdir(pathname);
-        if (ret == -1)
-            return (ft_putstr_fd("cd: No such file or directory\n", 2), 1);
+        pathname = remove_slash(path); 
         cd_path(sack, pathname, pwd, old_pwd);
-
     }
-
-    printf("PWD: %s\n", sack->env->pwd);
-    printf("OLDPWD: %s\n", sack->env->oldpwd);
+    // printf("PWD: %s\n", sack->env->pwd);
+    // printf("OLDPWD: %s\n", sack->env->oldpwd);
     export(sack->env, sack->env->pwd);
     export(sack->env, sack->env->oldpwd);
     free (pwd);
     free (old_pwd);
     return (0);
-
 }
